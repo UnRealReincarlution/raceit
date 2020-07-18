4c004493ab35e68b3d1c9e5759cca80e96c52c29
UnReal Ben <benwhite2413@gmail.com>
2020-07-18 10:09:37 +0000

    UI Updates and Server Updates

--- index.js
+++ index.js
@@ -7,45 +7,64 @@ const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')));
 
 rooms = [];
+connections = [];
+connection_index = [];
 
 io.on('connection', (socket) => {
-  //console.log(socket);
-  
-  var userId = "";
-  console.log(`User: ${userId} Connected`);
+  console.log(`+++++: ${socket.id} Connected`);
+  socket.username = "deafult";
+
+  connections.push(socket);
+  connection_index.push(socket.id);
+
+  socket.on('disconnect', function() {
+    console.log(`-----: ${socket.id} Disconnected`);
+  });
 
   socket.on('echo', (data) => {
-		socket.emit('echo', {
-			message: data
-		});
+    socket.to(data.room).emit('echo', {
+      message: data.data
+    });
 
-    console.log('Echoing', data);
-	});
+    console.log('Echoing', data.data);
+  });
 
   socket.on('create', (room, your_name) => {
     socket.join(room);
-    
+
     console.log(`Creating and Joining Room: ${room}`);
     rooms.push(room);
-    console.log(rooms);
-    console.log(io.nsps['/'].adapter.rooms); 
+
+    connections[connections.indexOf(socket)].host = true;
+    socket.host = true;
+
+    //console.log(rooms);
+    //console.log(io.nsps['/'].adapter.rooms);
   });
 
   socket.on('join', (room, name) => {
-    if(rooms.includes(room)) {
-      socket.join(room);
-    }else{
+    if (rooms.includes(room)) {
       socket.join(room);
     }
+
+    connections[connections.indexOf(socket)].host = false;
+    socket.host = false;
   });
 
   socket.on('getRooms', () => {
-      socket.emit('rooms', {
-        message: rooms
-      });
+    socket.emit('rooms', {
+      message: rooms
+    });
 
-      console.log("Get Rooms Request Recieved and Replied to");
-      console.log(rooms); 
+    console.log("Get Rooms Request Recieved and Replied to");
+    //console.log(rooms);
+  });
+
+  socket.on('setUsername', (username) => {
+    connections[connections.indexOf(socket)].username = username;
+    socket.username = username;
+
+    console.log(`Username set! - ${username}`);
   });
 
   socket.on('getRoomMembers', (room_name) => {
@@ -53,23 +72,25 @@ io.on('connection', (socket) => {
     sockets = [];
 
     for (socketID in io.nsps['/'].adapter.rooms[room_name].sockets) {
-      sockets.push(socketID);
+      sockets.push({name: connections[connection_index.indexOf(socketID)].username, id: socketID});
     }
 
-    socket.emit('postRoomMembers', {
+    io.in(room_name).emit('postRoomMembers', {
       message: sockets
     });
+
+    //console.log(sockets);
   });
 
   function getAvaliableRooms() {
     var roomKeys = Object.keys(socket.rooms);
     var socketIdIndex = roomKeys.indexOf(socket.id);
     console.log(socketIdIndex, " ", socket.id);
-    var rooms = roomKeys.splice( socketIdIndex, 1 );
+    var rooms = roomKeys.splice(socketIdIndex, 1);
     return rooms;
   }
 });
 
 server.listen(3000, function() {
-	console.log('Server listening at port %d', 3000);
+  console.log('Server listening at port %d', 3000);
 });
--- public/script.js
+++ public/script.js
@@ -1,101 +1,31 @@
 class race {
-    constructor(room, position = 0){
-        this.position = position;
-        this.room_name = room;
-    }
+  constructor(room, name = "invalid", position = 0) {
+    this.position = position;
+    this.room_name = room;
+    this.nickname = name;
 
-    moveForward() {
-        console.log("Itterating!");
-        this.position++;
-    }
+    socket.emit('setUsername', name);
+  }
 
-    getRoomName() {
-      return this.room_name;
-    }
+  moveForward() {
+    this.position++;
+
+    socket.to(this.room_name).emit('moving', {
+      position: this.position,
+      id: socket.id
+    });
+  }
+
+  getRoomName() {
+    return this.room_name;
+  }
+
+  getNickName() {
+    return this.nickname;
+  }
 }
-
-$(document).ready(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-
-    const hosting = urlParams.get('hosting');
-    console.log(hosting);
-
-    const room = urlParams.get('room_name');
-    console.log(room);
-
-    if(hosting == 'true' || hosting == 'false'){
-      joinRaceFunction(room);
-    }
-
-    if(!localStorage.getItem('themeSwitch')){
-      document.documentElement.setAttribute('theme', 'light');
-      localStorage.setItem('themeSwitch', 'light');
-    }else{
-      document.documentElement.setAttribute('theme', localStorage.getItem('themeSwitch'));
-    }
-});
 
 let race_;
-
-function hostRace() {
-  let room_name = $("#room_name_choice").val();
-  console.log(room_name);
-
-  socket.emit('create', room_name);
-  joinRaceFunction(room_name, true) 
-}
-
-function joinRace(room_name) {
-  socket.emit('join', room_name);
-  joinRaceFunction(room_name);
-}
-
-function joinRaceFunction(room_name, hosting = false) {
-  race_ = new race(room_name);
-  $("#game_name").html(race_.getRoomName());
-
-  $("#choose_field").addClass("hidden");
-  $("#race_div").removeClass("hidden");
-  $(".input_field").removeClass("active_pannel");
-  $("game_name").removeClass("hidden");
-
-  socket.emit('getRoomMembers', room_name);
-}
-
-function showHostOptions() {
-  $("#host_game").addClass("active_pannel");
-}
-
-function showJoinOptions() {
-  $("#join_game").addClass("active_pannel");
-  
-  socket.emit('getRooms', '');
-}
-
-let loadRooms = (data) => {
-  //console.log(data.message);
-  while(document.getElementById("room_list").firstChild){
-    document.getElementById("room_list").removeChild(document.getElementById("room_list").firstChild);
-  }
-  data.message.forEach(element => {
-    console.log(element);
-
-    var room_element = document.createElement("li");
-        room_element.setAttribute('game-join', element);
-        room_element.setAttribute('onclick', `joinRace("${element}")`);
-
-    var room_div = document.createElement("div");
-
-    var room_name = document.createElement("p");
-        room_name.innerHTML = element;
-        room_div.appendChild(room_name);
-    
-    room_element.appendChild(room_div);
-    document.getElementById("room_list").appendChild(room_element);
-  });
-}
-
 let socket = io();
 
 socket.on('echo', (data) => {
@@ -109,35 +39,137 @@ socket.on('rooms', (data) => {
 
 socket.on('postRoomMembers', (data) => {
   console.log('recieved room person data', data);
+  loadMembers(data);
 });
 
-function toggleTheme() {
-  var darkThemeSel = localStorage.getItem('themeSwitch') === 'dark';
-  console.log("Switching theme", darkThemeSel);
-
-  if(localStorage.getItem('themeSwitch')){
-    if(darkThemeSel){
-      document.documentElement.setAttribute('theme', 'light');
-      localStorage.setItem('themeSwitch', 'light');
-    }
-    else{
-      document.documentElement.setAttribute('theme', 'dark');
-      localStorage.setItem('themeSwitch', 'dark')
-    }
-  }else{
-      document.documentElement.setAttribute('theme', 'light');
-      localStorage.setItem('themeSwitch', 'light');
-  }
-}
-
 $("#host_game").click(function(e) {
-  if($(e.target).is("#host_game")){
+  if ($(e.target).is("#host_game")) {
     $("#host_game").removeClass("active_pannel");
   }
 })
 
 $("#join_game").click(function(e) {
-  if($(e.target).is("#join_game")){
+  if ($(e.target).is("#join_game")) {
     $("#join_game").removeClass("active_pannel");
   }
 })
+
+$(document).ready(() => {
+  if (!localStorage.getItem('themeSwitch')) {
+    document.documentElement.setAttribute('theme', 'light');
+    localStorage.setItem('themeSwitch', 'light');
+  } else {
+    document.documentElement.setAttribute('theme', localStorage.getItem('themeSwitch'));
+  }
+});
+
+function users_name() {
+  return prompt("Please enter your name");
+}
+
+function hostRace() {
+  let room_name = $("#room_name_choice").val();
+  let user_name = users_name();
+  console.log(room_name);
+
+  socket.emit('create', room_name);
+  joinRaceFunction(room_name, user_name, true)
+}
+
+function joinRace(room_name, user_name) {
+  user_name = users_name();
+  socket.emit('join', room_name);
+  joinRaceFunction(room_name, user_name);
+}
+
+function joinRaceFunction(room_name, username, hosting = false) {
+  race_ = new race(room_name, username);
+  $("#game_name").html(race_.getRoomName());
+
+  $("#choose_field").addClass("hidden");
+  $("#race_div").removeClass("hidden");
+  $(".input_field").removeClass("active_pannel");
+  $("#game_name").removeClass("hidden");
+
+  socket.emit('getRoomMembers', room_name);
+}
+
+function showHostOptions() {
+  $("#host_game").addClass("active_pannel");
+}
+
+function showJoinOptions() {
+  $("#join_game").addClass("active_pannel");
+
+  socket.emit('getRooms', '');
+}
+
+let loadRooms = (data) => {
+  //console.log(data.message);
+  while (document.getElementById("room_list").firstChild) {
+    document.getElementById("room_list").removeChild(document.getElementById("room_list").firstChild);
+  }
+  data.message.forEach(element => {
+    console.log(element);
+
+    var room_element = document.createElement("li");
+    room_element.setAttribute('game-join', element);
+    room_element.setAttribute('onclick', `joinRace("${element}")`);
+
+    var room_div = document.createElement("div");
+
+    var room_name = document.createElement("p");
+
+        element = element.replace(/>/g,"&#62;");
+        element = element.replace(/</g,"&#60;");
+
+        room_name.innerHTML = element;
+        room_div.appendChild(room_name);
+
+    room_element.appendChild(room_div);
+    document.getElementById("room_list").appendChild(room_element);
+  });
+}
+
+async function loadMembers(data) {
+  while (document.getElementById("member_list_dump").firstChild) {
+    document.getElementById("member_list_dump").removeChild(document.getElementById("member_list_dump").firstChild);
+  }
+
+  var user_count = data.length;
+  var user_cap = 15;
+  
+  $("#member_list_title").html(`Member List  <i>${user_count}/${user_cap}</i>`);
+
+  data.message.forEach(element => {
+    var this_div = document.createElement("li");
+
+        element.name = element.name.replace(/>/g,"&#62;");
+        element.name = element.name.replace(/</g,"&#60;");
+      
+        this_div.innerHTML = element.name;
+        this_div.setAttribute('userid', element.id);
+
+    $("#member_list_dump").append(this_div);
+  });
+}
+
+function toggleTheme() {
+  var darkThemeSel = localStorage.getItem('themeSwitch') === 'dark';
+  console.log("Switching theme", darkThemeSel);
+
+  if (localStorage.getItem('themeSwitch')) {
+    if (darkThemeSel) {
+      document.documentElement.setAttribute('theme', 'light');
+      localStorage.setItem('themeSwitch', 'light');
+    }
+    else {
+      document.documentElement.setAttribute('theme', 'dark');
+      localStorage.setItem('themeSwitch', 'dark')
+    }
+  } else {
+    document.documentElement.setAttribute('theme', 'light');
+    localStorage.setItem('themeSwitch', 'light');
+  }
+}
+
